b58d9e9d0d501a547cc731b82e407b85c1356793
Reinaldo Chavez <reinaldo.chavez.fonseca@gmail.com>
2022-12-13 23:17:53 -0300

    drop jwt

--- contactos-jwt/src/main/java/com/rchf/contactos/app/controller/AuthController.java
+++
@@ -1,8 +0,0 @@
-package com.rchf.contactos.app.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AuthController {
-    AuthenticationManager authenticationManager;
-}
--- contactos-jwt/src/main/java/com/rchf/contactos/app/utils/Constans.java
+++
@@ -1,9 +0,0 @@
-package com.rchf.contactos.app.utils;
-
-public class Constans {
-    public static final String KEY = "1234";
-    public static final long TIME_LIFE = 86_400_000; // 1 DIA
-    public static final String ENCABEZADO = "authorization";
-    public static final String TOKEN_PREFIX = "Bearer";
-
-}
